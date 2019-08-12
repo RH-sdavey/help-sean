2fb461f61c9c8bc61eb67f31a93bf9b8517638d2
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-12 23:04:58 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,17 @@ const IndexPage = () => (
         <p></p>
         <h2>What the fuck, why should I help you?</h2>
         <p>I'm just asking for help, you don't need to help me, I really dont mind if you have no time or just dont want to.. thats fine</p>
-        <p>But first jut let me explain what Im doing on the website...</p>
+        <p>But first just let me explain what Im doing on the website...</p>
+        <p> Sean Will Help You ( swhy.netlify.com ) is a free Linux tutorial website, that will be updated regularly with new tutorials that will teach visitors to the site how to do tasks in Linux</p>
+        <p> Everything from Sysadmin to devops and developer stuff... </p>
+        <p>Whatever we decide to put there...these tutorials can be easy/medium/hard/expert tasks, and while I am aiming 99% towards Linux, I already have one guy providing Windows tutorials..</p>
+        <p></p>
+        <h2>So...get to the point...</h2>
+        <p>I need content, the website structure is mostly done, I have a few things I need to do, but I need conteeentttt....</p>
+        <p>I need tutorials, written in Markdown language explaining how to do stuff in Linux, simple tasks like how to restart a service, show which port is being used by a process...</p>
+        <p> through to complicated stuff... how to port forward an ssh connection through a docker container in an ansible playbook... and everything in between </p>
+        <p> If you have lietrally any skill in IT and want to share that with the world, then please help me... </p>
+        
         
       </Container>
     </Page>
