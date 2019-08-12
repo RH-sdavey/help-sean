0bf3b9561fc55b48c24f241919684949c637016c
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-12 22:52:52 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,14 @@ const IndexPage = () => (
     <Page>
       <Container>
         <h1>Hello!</h1>
+        <h2>Sean...what do you want?</h2>
         <h2> I made this website... to ask you for help... with helping me to build another website :D </h2>
         <p>I would like to ask for your help to provide me with content for this website...</p>
         <a href="https://swhy.netlify.com/">Sean Will Help You</a>
+        <p></p>
         <p>All details on how to contribute are here...</p>
         <a href="https://github.com/RH-sdavey/sean-will-help-you">find the contributing.md file</a>
-        <h2>Sean...what do you want?</h2>
+        
       </Container>
     </Page>
   </IndexLayout>
