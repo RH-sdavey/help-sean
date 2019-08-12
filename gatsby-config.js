ef45e452e8707c502a8aab65831644826cba56bc
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-12 23:44:05 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-typescript-plus',
-    description: 'A starter kit for TypeScript-based Gatsby projects with sensible defaults.',
+    title: 'Thanks for clicking the link man....',
+    description: 'Asking for help',
     keywords: 'gatsbyjs, gatsby, javascript, sample, something',
     siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
     author: {
-      name: 'Resi Respati',
-      url: 'https://twitter.com/resir014',
-      email: 'resir014@gmail.com'
+      name: 'Sean',
+      url: 'https://github.com/RH-sdavey',
+      email: 'seankdavey@seznam.cz'
     }
   },
   plugins: [
